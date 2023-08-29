64b2e465215c661531c9f4b2f9750738bdaa8ca7
Parthibg00410769 <g00410769@atu.ie>
2023-08-30 00:44:18 +0100

    Quick_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,5 +9,5 @@ for j in range(len(list1)-1):
     for i in range(len(list1)-1):
         if list1[i] > list1[i+1]:
             list1[i], list1[i+1] = list1[i+1], list1[i]
-
-print(list1)
+        print(list1)
+#print(list1)
